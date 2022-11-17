b557cd20573ce5c093ab896bc9f9efd266b06001
siddheshpatil28 <d21124585@mytudublin.ie>
2022-11-17 20:25:10 +0000

    Added quiz components js files with all the questions and a build folder to complete

---
+++ SRC/Quiz App Src/build/precache-manifest.65762f945b0aefe1b266513ef9424ff1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e83672286bbf7397b624736c2705fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebda10b7f958b90e9d21",
+    "url": "/static/css/main.f84372a5.chunk.css"
+  },
+  {
+    "revision": "bcecc83d6ae2b5bcceec",
+    "url": "/static/js/2.873279ad.chunk.js"
+  },
+  {
+    "revision": "ebda10b7f958b90e9d21",
+    "url": "/static/js/main.4854c82a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
