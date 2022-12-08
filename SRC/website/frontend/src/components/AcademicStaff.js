2e3fd53540cbd94de725e1c2a03ad6a8d69d9ee4
Mehul Prajapati <prajapatimehul126@gmail.com>
2022-12-08 20:03:11 +0530

    Added Our Lecturers Dynamic Page

--- SRC/website/frontend/src/components/AcademicStaff.js
+++ SRC/website/frontend/src/components/AcademicStaff.js
@@ -1,17 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
 import {useSelector, useDispatch} from 'react-redux';
+import axios from 'axios';
 
-import {receiveSuccessMessage} from '../redux/util/controller';
+import {receiveSuccessMessage, receiveFailureMessage} from '../redux/util/controller';
 
 const AcademicStaff = () => {
 
-    const dispatch = useDispatch();
-    const courses = useSelector(state => state.course);
+  const dispatch = useDispatch();
+  const selectedCourse = useSelector(state => state.course.selected_course);
 
-    useEffect(() => {
-		window.scrollTo(0, 0);
-	}, []);
+  const [allProfessors, setAllProfessors] = useState([]);
+  const [staffs, setStaffs] = useState({});
+
+  useEffect(() => {
+    // make API call to fetch staffs
+    axios.get(`/api/professor?college=${selectedCourse.college}`)
+      .then(resp => {
+        // console.log("resp", resp);
+        if (resp.data.success) {
+          dispatch(receiveSuccessMessage({success: `Staff for college found`}));
+          setAllProfessors(resp.data.professor);
+        } else {
+          dispatch(receiveFailureMessage({failure: resp.data.message}));
+        }
+        
+      })
+      .catch(err => {
+        console.log("error", err);
+      })
+  }, []);
+
+  useEffect(() => {
+    console.log("allProfessors", allProfessors);
+    if (allProfessors && allProfessors.length) {
+      const filteredStaff = allProfessors.filter(item => item.college == selectedCourse.college);
+      // console.log("filteredStaff", filteredStaff);
+      if (filteredStaff && filteredStaff.length) {
+        setStaffs(filteredStaff[0]);
+      }
+    }
+  }, [allProfessors]);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <React.Fragment>
@@ -24,27 +57,17 @@ const AcademicStaff = () => {
       <div className="p-60">
           <div className="container">
             <div className="staff_container">
-                <div className="single_staff">
-                    <img src="/images/s1.png" alt="" />
-                    <p className="dept">Head of Research</p>
-                    <h3 className="staff_name">Dr Dympna O'Sullivan</h3>
-                    <p>dympna.osullivan@tudublin.ie</p>
-                    <p>+353-1-220-5634</p>
-                </div>
-                <div className="single_staff">
-                    <img src="/images/s2.png" alt="" />
-                    <p className="dept">Head of Research</p>
-                    <h3 className="staff_name">Dr Dympna O'Sullivan</h3>
-                    <p>dympna.osullivan@tudublin.ie</p>
-                    <p>+353-1-220-5634</p>
-                </div>
-                <div className="single_staff">
-                    <img src="/images/s3.png" alt="" />
-                    <p className="dept">Head of Research</p>
-                    <h3 className="staff_name">Dr Dympna O'Sullivan</h3>
-                    <p>dympna.osullivan@tudublin.ie</p>
-                    <p>+353-1-220-5634</p>
-                </div>
+              {staffs && staffs.staff && staffs.staff.length && staffs.staff.map(item => {
+                return (
+                  <div className="single_staff">
+                    <img src={item.professor_thumbnail_url ? item.professor_thumbnail_url : "https://dummyimage.com/350x220/333/fff.png"} alt="" />
+                    <p className="dept">{item.designation}</p>
+                    <h3 className="staff_name">{item.professor_name}</h3>
+                    <p>{item.email}</p>
+                    <p>{item.phone}</p>
+                  </div>
+                )
+              }) }
             </div>
           </div>
       </div>
