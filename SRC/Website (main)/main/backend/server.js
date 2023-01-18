293ac353009a1c6b8d43ac9a31345a85f183525b
Kaique Silva <kaiquehonorato@hotmail.com>
2023-01-18 13:12:17 +0000

    Set View Engine Pug & SEO Improvements

--- SRC/Website (main)/main/backend/server.js
+++ SRC/Website (main)/main/backend/server.js
@@ -18,7 +18,7 @@ server.use(express.urlencoded({ extended: true }));
 
 // CORS
 server.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', 'https://unisim.vercel.app/');
+	res.setHeader('Access-Control-Allow-Origin', 'https://unilife.ml/');
 	res.setHeader('Access-Control-Allow-Credentials', true);
 	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, PUT, POST, PATCH, DELETE');
@@ -48,7 +48,7 @@ const sess = {
 // For using secure cookies in production, but allowing for testing in development
 if (config.nodeEnv = 'production') {
 	server.set('trust proxy', 1) // trust first proxy
-	// sess.cookie.secure = true // serve secure cookies
+	sess.cookie.secure = true // serve secure cookies
 }
 server.use(session(sess));
 
@@ -57,7 +57,7 @@ server.use(session(sess));
 server.use('/api', userRoutes);
 
 // Use public folder as static server commented as we do not need static host
-// server.use(express.static('public'));
+server.use(express.static('public'));
 
 // Set view engine to pug
 server.set('view engine', 'pug');
